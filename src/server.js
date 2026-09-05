bb522f733fa72489447f03738bf23fe69c2c4508
PSax-Dev <chinonsopraise700@gmail.com>
2026-09-05 18:17:00 +0100

    intentinal mistake

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import calculateRoute from './routes/calculate.js'
+import calculateRoute from './routes/calculate.jsx'
 
 const app = express()
 const port = process.env.PORT || 5000
